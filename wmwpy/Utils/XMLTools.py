1775bff16ef422870985669c332f82efd8e12d3b
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-01-31 19:19:09 -0700

    fix comment checking

--- wmwpy/Utils/XMLTools.py
+++ wmwpy/Utils/XMLTools.py
@@ -1,11 +1,11 @@
 from lxml import etree
 
-def findTag(root, tag):
+def findTag(root : etree.Element, tag : str) -> etree.Element:
     element = 0
     curTag = ''
     for e in root:
         print(e.tag)
-        if isinstance(e, etree.Comment):
+        if e.tag is etree.Comment:
             continue
         curTag = e.tag
         if curTag == tag:
