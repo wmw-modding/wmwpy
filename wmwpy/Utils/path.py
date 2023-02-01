a0be8d65bbd95f84ae0c9549022338e145efcd9a
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-01-31 19:19:44 -0700

    add types

--- wmwpy/Utils/path.py
+++ wmwpy/Utils/path.py
@@ -1,14 +1,14 @@
 import os
 import pathlib
 
-def joinPath(*args):
+def joinPath(*args : str) -> str:
     path = ''
     for p in args:
         path = os.path.join(path, getRelPath(p))
         
     return path
 
-def getRelPath(path):
+def getRelPath(path : str) -> str:
     if path[0] in ['/', '\\']:
         return path[1:]
     return path
