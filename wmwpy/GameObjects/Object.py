2a4a42eaeb7d8b6606048afe2a5da55dffe84507
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-01-31 21:22:36 -0700

    start work on game objects

--- wmwpy/GameObjects/Object.py
+++ wmwpy/GameObjects/Object.py
@@ -2,4 +2,25 @@ from ..Utils import WaltexImage
 
 class Object():
     def __init__(this, gamepath : str, assets : str, object : str, ) -> None:
+        """Get game object. Game object is `.hs` file.
+
+        Args:
+            gamepath (str): Game path
+            assets (str): Assets path, relative to game path
+            object (str): Object file relative to assets path. Must be `.hs` file.
+        """
         pass
+    
+class Sprite():
+    pass
+
+    class Animation():
+        pass
+
+        class Frame():
+            pass
+
+class Shape():
+    class Point():
+        def __init__(self) -> None:
+            pass
