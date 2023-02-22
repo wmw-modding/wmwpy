f1fae635a0f4ebcbfea639475166112ee1d0b59c
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-02-22 16:28:01 -0700

    Update __init__.py

--- src/wmwpy/Utils/__init__.py
+++ src/wmwpy/Utils/__init__.py
@@ -1,5 +1,6 @@
-from .Waltex import WaltexImage
+from .Waltex import WaltexImage, Waltex
 from .Widgets import Widgets
 from . import ImageUtils
 from . import XMLTools
-
+from .path import joinPath
+from . import filesystem
