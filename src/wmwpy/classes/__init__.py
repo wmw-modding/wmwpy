da11b2faf5ade0546ddbeb1b773a4ac5f509f36f
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-03-04 16:33:11 -0700

    add Imagelist

--- src/wmwpy/classes/__init__.py
+++ src/wmwpy/classes/__init__.py
@@ -2,3 +2,6 @@ from .level import Level
 from .layout import Layout
 from .sprite import Sprite
 from .object import Object
+from .Imagelist import Imagelist
+
+__all__ = ['Level', 'Layout', 'Sprite', 'Object', 'Imagelist']
