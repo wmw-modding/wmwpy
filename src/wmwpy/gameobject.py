f534eb0a702da9c90fc8b20bc2f07a60f278063c
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-03-06 15:09:57 -0700

    add GameObject
    This is to be used as a base for game objects, such as Level, Object, Imagelist, etc.

---
+++ src/wmwpy/gameobject.py
@@ -0,0 +1,36 @@
+from .Utils.filesystem import *
+
+import io
+
+class GameObject():
+    def __init__(this, filesystem : Filesystem | Folder = None, gamepath : str = None, assets : str = '/assets') -> None:
+        this.gamepath = gamepath
+        this.assets = assets
+        if this.assets == None:
+            this.assets = '/assets'
+        
+        try:
+            this.filesystem = filesystem
+            if isinstance(this.filesystem, Filesystem):
+                this.gamepath = this.filesystem.gamepath
+                this.assets = this.filesystem.assets
+                
+            elif isinstance(this.filesystem, Folder):
+                pass
+
+            else:
+                this.filesystem = Filesystem(this.gamepath, this.assets)
+                this.filesystem.getAssets()
+        except:
+            raise FileNotFoundError('Must have a valid `filesystem` or `gamepath`')
+    
+    def test_file(this, file) -> io.BytesIO | str:
+        if isinstance(file, bytes):
+            file = io.BytesIO(file)
+        elif isinstance(file, File):
+            file = file.rawcontent
+        elif not hasattr(file, 'read') and not isinstance(file, str):
+            raise TypeError(f"file can only 'str', 'bytes', or file-like object.")
+    
+        return file
+    
