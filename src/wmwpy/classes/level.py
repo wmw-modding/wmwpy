1b9dd3e59624af65b2a2a98617d4ceebf73fa667
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-03-06 10:36:59 -0700

    start work on loading level

--- src/wmwpy/classes/level.py
+++ src/wmwpy/classes/level.py
@@ -1,7 +1,42 @@
-import lxml
+import io
 from lxml import etree
 
+from ..Utils.filesystem import *
 
 class Level():
-    def __init__(self, xml = None, image = None) -> None:
+    def __init__(this, xml : str | bytes | File | etree.ElementBase, image : str | bytes | File , filesystem : Filesystem | Folder = None, gamepath : str = None, assets : str = '/assets') -> None:
+        this.gamepath = gamepath
+        this.assets = assets
+        if this.assets == None:
+            this.assets = '/assets'
+        
+        try:
+            this.filesystem = filesystem
+            if isinstance(this.filesystem, Filesystem):
+                this.gamepath = this.filesystem.gamepath
+                this.assets = this.filesystem.assets
+                
+            elif isinstance(this.filesystem, Folder):
+                pass
+
+            else:
+                this.filesystem = Filesystem(this.gamepath, this.assets)
+                this.filesystem.getAssets()
+        except:
+            raise FileNotFoundError('Must have a valid `filesystem` or `gamepath`')
+        
+        if isinstance(xml, bytes):
+            this.xml_file = io.BytesIO(xml)
+        elif isinstance(xml, File):
+            this.xml_file = xml.rawcontent
+        elif not hasattr(xml, 'read') and not isinstance(xml, str):
+            raise TypeError(f"file can only 'str', 'bytes', or file-like object.")
+        
+        this.xml = etree.parse(this.xml_file)
+        
+        this.objects = []
+        
+    
+    def read(this):
         pass
+
