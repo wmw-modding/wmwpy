d43b30a613d70f0123efe64f135b933a8605b92f
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-03-09 21:43:03 -0700

    load level

--- src/wmwpy/classes/level.py
+++ src/wmwpy/classes/level.py
@@ -2,6 +2,7 @@ import io
 from lxml import etree
 
 from ..Utils.filesystem import *
+from .object import Object
 
 class Level():
     def __init__(this, xml : str | bytes | File | etree.ElementBase, image : str | bytes | File , filesystem : Filesystem | Folder = None, gamepath : str = None, assets : str = '/assets') -> None:
@@ -32,11 +33,74 @@ class Level():
         elif not hasattr(xml, 'read') and not isinstance(xml, str):
             raise TypeError(f"file can only 'str', 'bytes', or file-like object.")
         
-        this.xml = etree.parse(this.xml_file)
+        this.xml = etree.parse(this.xml_file).getroot()
         
         this.objects = []
+        this.properties = {}
+        this.room = (0,0)
         
+        this.read()
     
     def read(this):
-        pass
+        this.objects = []
+        this.properties = {}
+        
+        for element in this.xml:
+            # comment safe-guard
+            if element is etree.Comment:
+                continue
+            
+            if element.tag == 'Object':
+                properties = {}
+                pos = (0,0)
+                name = element.get('name')
+                
+                for el in element:
+                    if not el is etree.Comment:
+                        if el.tag == 'AbsoluteLocation':
+                            pos = el.get('value')
+                        
+                        elif el.tag == 'Properties':
+                            for property in el:
+                                if not property is etree.Comment and property.tag == 'Property':
+                                    properties[property.get('name')] = property.get('value')
+                
+                obj = Object(
+                    this.filesystem.get(properties['Filename']), # get file because `Object` does not take filepath
+                    filesystem = this.filesystem,
+                    properties = properties,
+                    pos = pos,
+                    name = name,
+                )
+                
+                this.objects.append(obj)
+            
+            if element.tag == 'Properties':
+                for el in element:
+                    if el is etree.Comment:
+                        continue
+                    
+                    if el.tag == 'Property':
+                        this.properties[el.get('name')] = el.get('name')
+            
+            if element.tag == 'Room':
+                for el in element:
+                    if el is etree.Comment:
+                        continue
+                    
+                    if el.tag == 'AbsoluteLocation':
+                        this.room = tuple([float(_) for _ in el.get('value').split(' ')])
+    
+    def getObject(this, name : str):
+        """
+        Get object by name
 
+        Args:
+            name (str): Object name.
+        """
+        
+        for obj in this.objects:
+            if obj.name == name:
+                return obj
+        
+        return None
