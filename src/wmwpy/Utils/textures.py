11a4d80b061cdc83a7fc08676feef91964c672af
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-03-24 21:29:26 -0600

    add docstrings

--- src/wmwpy/Utils/textures.py
+++ src/wmwpy/Utils/textures.py
@@ -1,10 +1,7 @@
 import os
-import pathlib
-import numpy
 from lxml import etree
 from .waltex import WaltexImage, Waltex
 from PIL import Image
-import io
 
 from .path import joinPath
 from .XMLTools import findTag
@@ -14,6 +11,14 @@ _cachedWaltextImages = {}
 
 class Texture():
     def __init__(this, image : Image.Image | Waltex | File) -> None:
+        """Texture for image.
+
+        Args:
+            image (Image.Image | Waltex | File): Image object. Can be PIL.Image.Image, Waltex image, or file.
+
+        Raises:
+            TypeError: image must be PIL.Image.Image, Waltex, or filesystem.File.
+        """
         this._image = image
         
         if isinstance(this._image, File):
@@ -24,19 +29,38 @@ class Texture():
         elif isinstance(this._image, Image.Image):
             this.image = this._image
         else:
-            raise TypeError('image must be PIL.Image.Image or Waltex')
+            raise TypeError('image must be PIL.Image.Image, Waltex, or filesystem.File.')
         
     @property
     def size(this):
         return this.image.size
 
-def getHDFile(file):
+def getHDFile(file : str) -> str:
+    """Get HD filename.
+
+    Args:
+        file (str): Filename
+
+    Returns:
+        str: HD filename.
+    """
     split = os.path.splitext(file)
     split = list(split)
     split.insert(1, '-HD')
     return ''.join(split)
             
 def getTexture(path : str, textureSettings : dict, size : tuple, cache = True) -> Image.Image:
+    """Get image.
+
+    Args:
+        path (str): Path to Image.
+        textureSettings (dict): Texture settings.
+        size (tuple[width,height]): Size of image.
+        cache (bool, optional): Whether to cache waltex images. Defaults to True.
+
+    Returns:
+        Image.Image: PIL Image.
+    """
     type = os.path.splitext(path)[1][1:]
     image = None
     if type == 'waltex':
