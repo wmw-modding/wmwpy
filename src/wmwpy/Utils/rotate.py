1e5659375be768f9a9377967a051bbd9e8695c27
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-05-01 11:20:28 -0600

    Update rotate.py

--- src/wmwpy/Utils/rotate.py
+++ src/wmwpy/Utils/rotate.py
@@ -1,10 +1,21 @@
 import numpy
 
 
-def rotate(p, origin=(0, 0), degrees=0):
+def rotate(point, origin=(0, 0), degrees=0):
     angle = numpy.deg2rad(degrees)
-    R = numpy.array([[numpy.cos(angle), -numpy.sin(angle)],
-                  [numpy.sin(angle),  numpy.cos(angle)]])
-    # o = numpy.atleast_2d(origin)
-    m = numpy.dot(R, p)
-    return float(m.T[0]), float(m.T[1])
+    
+    point = numpy.array(point)
+    origin = numpy.array(origin)
+    
+    shape = point.shape
+    
+    if len(shape) > 1 and shape[1] >= 2:
+        return numpy.array([rotate(p, degrees = degrees, origin = origin) for p in point])
+
+    adjusted = (point - origin)
+    
+    cos_sin = numpy.array([numpy.cos(angle),numpy.sin(angle)])
+    
+    result = adjusted + cos_sin * adjusted[0] + numpy.flip(cos_sin) * adjusted[1]
+
+    return result
