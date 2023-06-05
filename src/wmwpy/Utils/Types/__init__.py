c55bcaf0e4ac5959329aeba7bd9fe59a6d369471
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-06-05 14:21:02 -0600

    Delete src/wmwpy/Utils directory because it apparently didn't get deleted when I originally deleted it.

--- src/wmwpy/Utils/Types/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import Documents
-from . import Images
-from .. import filesystem
-
-TYPES = Images.TYPES + Documents.TYPES
-
-for type in TYPES:
-    filesystem.register_reader(type)
--- src/wmwpy/Utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from .textures import Texture
-from . import textures
-from .waltex import WaltexImage, Waltex
-from . import XMLTools
-from .path import joinPath
-from . import filesystem
-from .filesystem import Filesystem
-from . import Types
-from .gif import save_transparent_gif
