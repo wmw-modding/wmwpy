25ab20733ac5182052beacfc1c403c4609dc5494
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-06-30 12:17:51 -0600

    make register class check better

--- src/wmwpy/classes/objectpack/pack.py
+++ src/wmwpy/classes/objectpack/pack.py
@@ -62,7 +62,7 @@ class ObjectPack():
         if not isinstance(object_type, type):
             object_type = object_type.__class__
         
-        if object_type.__base__ == Type or object_type == Type:
+        if issubclass(object_type, Type):
             self.types[object_type.NAME] = object_type
         else:
             raise TypeError('type must inherit from the classes.objectpack.type.Type class')
